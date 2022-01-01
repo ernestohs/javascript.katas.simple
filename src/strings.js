7d8ab91c525fff2e38a0f3eaac95e402d0ee9729
Ernesto Herrera Salinas <d03f39190777ff8411dd@yandex.com>
2021-12-31 21:27:51 -0600

    Add travis configuration

--- src/strings.js
+++ src/strings.js
@@ -6,14 +6,18 @@ export function longest(strings) {
 }
 
 export function stringMode(text) {
-  const {max, ...counts} = (text || "").split("").reduce(
+  const { max, ...counts } = (text || "").split("").reduce(
     (a, c) => {
-        a[c] = a[c] ? a[c] + 1 : 1;
-        a.max = a.max < a[c] ? a[c] : a.max;
-        return a;
+      a[c] = a[c] ? a[c] + 1 : 1;
+      a.max = a.max < a[c] ? a[c] : a.max;
+      return a;
     },
     { max: 0 }
-    );
+  );
 
-    return Object.entries(counts).filter(([k, v]) => v === max)[0][0];
+  return Object.entries(counts).filter(([k, v]) => v === max)[0][0];
+}
+
+export function anagram(a, b) {
+
 }
