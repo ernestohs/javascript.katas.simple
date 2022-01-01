375c3c0e7b870d9520f57b6113a9ae8fa10efc6d
Ernesto Herrera Salinas <d03f39190777ff8411dd@yandex.com>
2021-12-31 19:31:45 -0600

    Implement longest string in an array - Implement function to find longest string - Add unit tests following wiki guidelines - Configure test cases - Configure scripts for test/debug - Add Babel configuration - Install Jest packages - Configure vscode for debugging

--- src/strings.js
+++ src/strings.js
@@ -1,3 +1,6 @@
-export function longest () {
-    
+export function longest(strings) {
+  if (strings.length === 0) return;
+  let max = strings[0].length;
+  strings.map((v) => (max = Math.max(max, v.length)));
+  return (strings.filter((v) => v.length == max) || [])[0];
 }
--- src/strings.spec.js
+++ src/strings.spec.js
@@ -1,6 +1,17 @@
+import { longest } from "./strings";
+import { default as longStringCases } from "../cases/longstring.cases.json";
 
-describe('String Katas', () => {
-    describe('Write a function that accepts an array of strings. Return the longest string.', ()=>{
-        
-    })
-})
+describe("String Katas", () => {
+  describe("Write a function that accepts an array of strings. Return the longest string.", () => {
+    longStringCases.forEach((testCase) => {
+      it(`should return "${
+        testCase.expected
+      }" as the longest string from this array ${JSON.stringify(
+        testCase.input
+      )} `, () => {
+        const actual = longest(testCase.input);
+        expect(actual).toBe(testCase.expected);
+      });
+    });
+  });
+});
